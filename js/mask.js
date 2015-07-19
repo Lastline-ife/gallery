96d88c7642bffa07e716dd70860bc6ebd8f408e2
slipx <xiongty@outlook.com>
2015-07-19 17:04:30 +0800

    小调整，主要是把蒙板显示不显示放到#mask-img控制，另外就是代码的风格调整～～Y(^_^)Y

--- js/mask.js
+++ js/mask.js
@@ -1,15 +1,13 @@
- function showDiv() {  
-       var doc= document,
-        sWidth=doc.body.scrollWidth,
-	    sHeight=doc.body.scrollHeight;
-       var   oMask= doc.getElementById('popWindow'); 
-           oMask.style.display = 'block';  
-           doc.getElementById('maskLayer').style.display = 'block';
-           oMask.style.height= sHeight+"px";
-           oMask.style.width= sWidth+"px";
-    };  
- function closeDiv() { 
-      var doc= document;
-        doc.getElementById('popWindow').style.display = 'none';  
-        doc.getElementById('maskLayer').style.display = 'none';  
-    };  
+function showDiv() {
+    var doc = document,
+        sWidth = doc.body.scrollWidth,
+        sHeight = doc.body.scrollHeight;
+    var oMask = doc.getElementById('popWindow');
+    doc.getElementById('mask-img').style.display = 'block';
+    oMask.style.height = sHeight + "px";
+    oMask.style.width = sWidth + "px";
+}
+function closeDiv() {
+    var doc = document;
+    doc.getElementById('mask-img').style.display = 'none';
+}
