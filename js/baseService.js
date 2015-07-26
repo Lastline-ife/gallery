50c0a6575f1e9b9c4a03de12482c139e324f090e
starkwang <381152119@qq.com>
2015-07-26 21:43:00 +0800

    add BaseService

--- js/baseService.js
+++ js/baseService.js
@@ -1,9 +1,9 @@
 AV.initialize("noew3oh7gutlboqz4vh7cpbh26zjxchsdrjd75kzl8pv26t0", "t5h3fn0qcdfxssnxjnn5enigdu4ii07gm223jlltjv0z3411");
 
-
 (function() {
     var Picture = AV.Object.extend("Picture");
     var _File = AV.Object.extend("_File");
+    var allCategoryName = AV.Object.extend("allCategoryName");
 
     /**
      * 获取图片的URL，数组形式返回给回调函数
@@ -45,10 +45,63 @@ AV.initialize("noew3oh7gutlboqz4vh7cpbh26zjxchsdrjd75kzl8pv26t0", "t5h3fn0qcdfxs
             }, function(error) {
                 alert('The file either could not be read, or could not be saved to AV.'+ error);
             });
+
+        refreshCategory(category);
+        
     }
 
 
+    /**
+     * 传入一个类别的名称，检查这个类别是否存在，若不存在的话添加这个类别
+     * @param {string} categoryName
+     */
+    function refreshCategory(categoryName){
+        //设置类别，如果是新类别的话就添加
+        getAllCategoryName(function(result){
+            if(result.indexOf(categoryName) == -1){
+                addCategory(categoryName);
+            }
+        })
+    }
+    /**
+     * 获取全部专辑名字
+     * @param {function} callback
+     */    
+     function getAllCategoryName(callback){
+        var query = new AV.Query(allCategoryName);
+        query.equalTo("name", "allCategoryName");
+        query.find({
+            success: function(results) {
+                console.log("Successfully retrieved " + results.length + " posts.");
+                // 处理返回的结果数据
+                var result = results[0].get('allCategoryName');
+                callback(result);
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    }
 
+
+    function addCategory(newCategoryName){
+        var query = new AV.Query(allCategoryName);
+        query.equalTo("name", "allCategoryName");
+        query.find({
+            success: function(results) {
+                console.log("Successfully retrieved " + results.length + " posts.");
+                // 处理返回的结果数据
+                var nowCategory = JOSN.parse(results[0].get('allCategoryName'));
+                nowCategory.push(newCategoryName)
+                var newCategory = JSON.stringify(nowCategory);
+                results[0].set('allCategoryName',newCategory);
+                results[0].save();
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    }
 
     window.baseService = {
         getPicturesByCategory: getPicturesByCategory,
